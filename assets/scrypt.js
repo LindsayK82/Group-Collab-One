808bc8cd253767ced963af2ea331efe71d4127d9
Yosef Teshome <menelik08@gmail.com>
2022-11-07 15:05:04 -0500

    initial js api link code

--- assets/scrypt.js
+++ assets/scrypt.js
@@ -1,2 +1,16 @@
+// put before api links https://russelldev-cors-anywhere.herokuapp.com/
+
+
+
+
+
+fetch(`https://russelldev-cors-anywhere.herokuapp.com/https://any.ge/horoscope/api/?sign=aquarius&type=daily&day=today`)
+.then(response => response.json())
+.then(data => {
+    console.log(data)
+});
+
+
+
 
 
